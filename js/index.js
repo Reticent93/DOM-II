2b3a79e78883109685b4d00276f35694ffb649ee
Greg Renfro <remph819@gmail.com>
2019-08-09 21:11:00 -0700

    created 3 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const heading = document.querySelector('.logo-heading')
+heading.addEventListener('mouseover', (event)=> {
+    event.currentTarget.style.color = 'blue'
+})
+
+const home = document.querySelector('.nav-link')
+home.addEventListener('keydown', (e)=> {
+    e.currentTarget.style.backgroundColor = "black"
+})
+
+const uncontent = document.querySelector('.intro')
+uncontent.addEventListener('mouseout', (e) => {
+e.currentTarget.style.backgroundColor = 'orange'
+})
+
+const happy = document.querySelector('.')
+
+happy.addEventListener('scroll', (e) => {
+    e.currentTarget
+})
